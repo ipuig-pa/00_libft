5cfd915e6e8d4ec9c053354ab82974f8cdce5fa0
Irene Puig Parnau <ipuig-pa@1-C-17.42heilbronn.de>
2024-10-09 11:47:21 +0200

    ft_memset.c added

---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 10:56:14 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2024/10/09 11:46:57 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*p;
+	size_t			i;
+
+	p = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		b[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
