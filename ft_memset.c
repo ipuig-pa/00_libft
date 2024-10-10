a07b4370b5200edd001cd01471c4d0783f9f12c1
Irene Puig Parnau <ipuig-pa@1-B-21.42heilbronn.de>
2024-10-10 12:23:16 +0200

    ft_memset.c with main test for printable

--- ft_memset.c
+++ ft_memset.c
@@ -6,9 +6,14 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 10:56:14 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/10/09 11:54:59 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/10/10 12:22:47 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*#include <stdio.h>
+#include <string.h>
+*/
+//This main does not test for the behavior with non printable characters
 
 void	*ft_memset(void *b, int c, size_t len)
 {
@@ -24,3 +29,15 @@ void	*ft_memset(void *b, int c, size_t len)
 	}
 	return (b);
 }
+/*
+int	main(void)
+{
+	char	b1[15] = "Hello world";
+	char	b2[15] = "Hello world";
+	int		c = 56;
+	size_t	len = 4;
+
+	printf("you: %s, memcpy: %s", ft_memset(b1, c, len), memset(b2, c, len));
+	return (0);
+}
+*/
