40876f739d3ad14076629bcb28fe823bc2e2bd8e
Irene Puig Parnau <ipuig-pa@1-B-21.42heilbronn.de>
2024-10-10 12:33:18 +0200

    ft_bzero.c added with main

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,21 +6,43 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 11:47:55 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/10/09 11:52:51 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/10/10 12:32:54 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*#include <stdio.h>
+#include <string.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*p;
+	size_t			i;
+
+	p = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		p[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+*/
+
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-
-	if (n != 0)
-	{
-		i = 0;
-		while (i < n)
-		{
-			s[i] = 0;
-			i++;
-		}
-	}
+	ft_memset(s, 0, n);
 }
+/*
+int	main(void)
+{
+	char	b1[15] = "Hello world";
+	char	b2[15] = "Hello world";
+	size_t	len = 4;
+
+	ft_bzero(b1, len);
+	bzero(b2, len);
+	printf("you: %c, %c, %c, %c, %c, bzero: %c, %c, %c, %c, %c", b1[0], b1[1], b1[2], b1[3], b1[4], b2[0], b2[1], b2[2], b2[3], b2[4]);
+	return (0);
+}
+*/
