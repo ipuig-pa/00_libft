09af522358a4bf97d32011bb1c6dbc5dd84b98d5
Irene Puig Parnau <ipuig-pa@1-B-21.42heilbronn.de>
2024-10-10 14:31:28 +0200

    ft_strchr.c added

---
+++ ft_strchr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 14:07:03 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2024/10/10 14:30:52 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <string.h>
+#include <stdio.h>*/
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (unsigned char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+/*int	main(void)
+{
+	char	s[] = "Hello World!";
+	int 	c = 65;
+
+	printf("you: %c, strchr: %c", *ft_strchr(s, c), *strchr(s, c));
+}*/
