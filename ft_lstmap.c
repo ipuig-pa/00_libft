45e87a56063899d66cf4c9cdc1b3b88fb73d52ea
Irene Puig Parnau <ipuig-pa@1-B-13.42heilbronn.de>
2024-10-25 16:04:34 +0200

    ft_lstmap.c moified, not finished yet

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,22 +6,31 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:25:09 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/10/25 15:29:42 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/10/25 16:03:52 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+//NOT FINISHED!!!! NOT WORKING!!!!
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*current;
+	t_list	*new;
+	t_list	*new_list;
 
 	if (!lst || !f || !del)
-		return ;
+		return (NULL);
 	current = lst;
 	while (current != NULL)
 	{
-		f(current->content);
+		new = malloc(sizeof(t_list));
+		if (!new)
+			return (NULL);
+		new->content = f(current->content);
+		new->next = 
+		new_list = ft_lstnew(new);
+		ft_lstadd_back(&new_list, new);
 		current = current->next;
 	}
 }
